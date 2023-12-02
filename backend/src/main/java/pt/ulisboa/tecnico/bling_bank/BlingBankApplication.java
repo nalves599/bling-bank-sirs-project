09c76036f99e3a17bf544237ab47ce3c7561bdb6
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-12-02 18:31:20 +0000

    Add minimal configuration of spring boot

--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/BlingBankApplication.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/BlingBankApplication.java
@@ -2,8 +2,10 @@ package pt.ulisboa.tecnico.bling_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class BlingBankApplication {
 
 	public static void main(String[] args) {
