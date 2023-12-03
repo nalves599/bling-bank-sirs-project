3a1d7f010e1e190baad40dc2e27b54439a031291
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-12-03 14:20:46 +0000

    Add controllers

--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/AccountController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/AccountController.java
@@ -1,8 +1,11 @@
 package pt.ulisboa.tecnico.bling_bank.account.controller;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.bling_bank.account.service.AccountService;
+
+import java.util.Set;
 
 @RestController
 public class AccountController {
@@ -10,20 +13,19 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-    /*
     @PostMapping("/accounts/create")
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account.getBalance(), account.getCurrencyType());
-    }
-    
-     */
+    public JSONObject createAccount(@RequestBody String body) {
+        JSONObject json = new JSONObject(body);
+        int balance = json.getInt("balance");
+        String currency = json.getString("currency");
+        Set<String> holders = Set.of(json.getString("holderName"));
+        return accountService.createAccount(balance, currency, holders);
 
-    /*
-    
+    }
+
     @GetMapping("/accounts/{id}")
-    public Account getAccount(@PathVariable Long id) {
+    public JSONObject getAccount(@PathVariable Long id) {
         return accountService.getAccount(id);
     }
-    
-     */
+
 }
--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/MovementController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/MovementController.java
@@ -1,8 +1,11 @@
 package pt.ulisboa.tecnico.bling_bank.account.controller;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.bling_bank.account.service.MovementService;
+
+import java.util.Date;
 
 @RestController
 public class MovementController {
@@ -10,11 +13,23 @@ public class MovementController {
     @Autowired
     private MovementService movementService;
 
-    /*
-    @GetMapping("/accounts/{id}/movements")
-    public List<Movement> getMovementsPerAccount(@PathVariable Long id) {
-        return movementsService.getMovementsPerAccount(id);
+    @PostMapping("/movements/create")
+    public JSONObject createMovement(@RequestBody String body) {
+        JSONObject json = new JSONObject(body);
+        Date date = new Date(json.getString("date"));
+        int value = json.getInt("value");
+        String description = json.getString("description");
+        Long accountId = json.getLong("accountId");
+        return movementService.createMovement(date, value, description, accountId);
     }
-    
-     */
+
+    @GetMapping("/movements/{id}")
+    public JSONObject getMovement(@PathVariable Long id) {
+        return movementService.getMovement(id);
+    }
+
+    @GetMapping("/movements/account/{id}")
+    public JSONObject getMovementByAccount(@PathVariable Long id) {
+        return movementService.getMovementByAccount(id);
+    }
 }
