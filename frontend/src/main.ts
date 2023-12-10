e668cf2d4d65db9a345005509477974b763da837
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-12-10 16:08:11 +0000

    Start adding JWT to frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -15,10 +16,14 @@ const vuetify = createVuetify({
   directives
 })
 
+const pinia = createPinia()
+
 const app = createApp(App)
 
 app.use(router)
 
 app.use(vuetify)
+
+app.use(pinia)
 
 app.mount('#app')
