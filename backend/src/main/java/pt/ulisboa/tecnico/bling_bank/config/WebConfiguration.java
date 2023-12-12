914dd7692fbe4eeb2e0bbfbe6927b71f0ba30479
Diogo Melita <dgmelita9@gmail.com>
2023-12-12 17:38:21 +0000

    stuff

--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/config/WebConfiguration.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/config/WebConfiguration.java
@@ -4,14 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.xml.transform.Source;
 import java.util.List;
 
 @Configuration
