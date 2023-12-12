d1daeaf114556bb7ae69aeb841151113bc87435c
Diogo Melita <dgmelita9@gmail.com>
2023-12-12 17:36:34 +0000

    stuff

--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/config/WebConfiguration.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/config/WebConfiguration.java
@@ -1,15 +1,36 @@
 package pt.ulisboa.tecnico.bling_bank.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.xml.transform.Source;
+import java.util.List;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+@EnableWebSecurity
+public class WebConfiguration {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-    @Override
-    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-            .allowedHeaders("*");
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> {});
+        return http.build();
     }
 }
