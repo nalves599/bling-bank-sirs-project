72844034087e304c35f90742b03f35419404ecc7
Diogo Melita <dgmelita9@gmail.com>
2023-12-13 12:10:08 +0000

    fix endpoint

--- backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/MovementController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/bling_bank/account/controller/MovementController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.bling_bank.account.service.MovementService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RestController
@@ -16,11 +18,25 @@ public class MovementController {
     @PostMapping("/movements/create")
     public String createMovement(@RequestBody String body) {
         JSONObject json = new JSONObject(body);
-        Date date = new Date(json.getString("date"));
-        int value = json.getInt("value");
-        String description = json.getString("description");
-        Long accountId = json.getLong("accountId");
-        return movementService.createMovement(date, value, description, accountId);
+
+        // Get the date string from the JSON object
+        String dateString = json.getString("date");
+
+        try {
+            // Parse the date string using SimpleDateFormat
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = dateFormat.parse(dateString);
+
+            int value = json.getInt("value");
+            String description = json.getString("description");
+            Long accountId = json.getLong("accountId");
+
+            return movementService.createMovement(date, value, description, accountId);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            // Handle the parsing exception as needed
+            return "Error parsing date";
+        }
     }
 
     @GetMapping("/movements/{id}")
