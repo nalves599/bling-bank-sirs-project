910961bedec7587df11e40b1bbe8a2814e8a733e
Diogo Melita <dgmelita9@gmail.com>
2023-12-13 11:33:40 +0000

    add dropdown menu to select account on movements page

--- frontend/src/views/MovementsView.vue
+++ frontend/src/views/MovementsView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="account-selector">
-    <label for="accountDropdown">Select Account:</label>
-    <select v-model="selectedAccount" id="accountDropdown" @change="fetchAccountsFromHolder">
+    <label for="accountDropdown" style="color: white; font-size: 18px;">Select Account:</label>
+    <select v-model="selectedAccountId" id="accountDropdown" @change="fetchAccountsFromHolder" style="width: 500px; background-color: #fff; color: #333; border: 1px solid #ccc; padding: 10px; border-radius: 5px;">
       <option value="" disabled>Select an account</option>
-      <option v-for="account in accounts" :key="account.accountId" :value="account.balance">
-        {{ account.accountId }}
+      <option v-for="account in sortedAccounts" :key="account.accountId" :value="account.accountId" style="background-color: #f8f8f8;">
+        {{ account.accountId }} - Balance: {{ account.balance }} - Holders: {{ account.holders.join(', ') }}
       </option>
     </select>
 
     <!-- Display movements or other components based on selected account -->
-    <div v-if="selectedAccount">
-      <h2>Movements for {{ selectedAccount }}</h2>
-      <!-- Add code to display movements here -->
+    <div v-if="selectedAccount" class="account-details">
+      <h2>Details for Account {{ selectedAccount.accountId }}</h2>
+      <p>Balance: {{ selectedAccount.balance }}</p>
+      <p>Holders: {{ selectedAccount.holders.join(', ') }}</p>
+      <!-- Add code to display movements or other details here -->
     </div>
 
     <BottomBar />
@@ -22,16 +24,14 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
 import { storeToRefs } from 'pinia'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
 import { getAccountsFromHolder } from '@/services/api'
 import type { AccountDto } from '@/models/AccountDto'
 import BottomBar from '@/components/BottomBar.vue'
 import LogoutButton from '@/components/LogoutButton.vue'
 
 const accounts = ref<AccountDto[]>([]) // Add AccountDto type definition
-const selectedAccount = ref<string>('')
-//const movements = ref<MovementDto[]>([])
-
+const selectedAccountId = ref<string | null>(null)
 const authStore = useAuthStore()
 const { username } = storeToRefs(authStore)
 
@@ -39,22 +39,41 @@ async function fetchAccountsFromHolder() {
   accounts.value = await getAccountsFromHolder(username.value)
 }
 
-fetchAccountsFromHolder()
+onMounted(() => {
+  fetchAccountsFromHolder()
+});
 
-/*async function fetchMovements() {
-  // Fetch movements for the selected account
-  if (selectedAccount.value) {
-    movements.value = await getAccountMovements(selectedAccount.value)
+const sortedAccounts = computed(() => {
+  return [...accounts.value].sort((a, b) => {
+    const idA = parseInt(a.accountId);
+    const idB = parseInt(b.accountId);
+    return idA - idB;
+  });
+});
+
+// Watcher to ensure selected option stays in the dropdown
+watch(() => selectedAccountId.value, (newVal) => {
+  if (newVal) {
+    selectedAccount.value = accounts.value.find(account => account.accountId === newVal) || null;
   }
-}*/
+});
+
+const selectedAccount = ref<AccountDto | null>(null);
+
 </script>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .account-selector {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center; /* Center content vertically */
+  }
+
+  .account-details {
+    text-align: center;
   }
 }
 </style>
