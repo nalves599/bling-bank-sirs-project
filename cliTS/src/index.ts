cd8dd8ed0244510680eae3cd5fa7584d6ff3c924
Tomás Esteves <tomasesteves2002@tecnico.ulisboa.pt>
2023-12-15 13:07:17 +0000

    Add cli

---
+++ cliTS/src/index.ts
@@ -0,0 +1,5 @@
+// Retrieve the main arguments (excluding the Node.js executable path and script file)
+const mainArgs = process.argv.slice(2);
+
+// Print the main arguments
+console.log('Main Arguments:', mainArgs);
